76aa9210b6674a6ba5aeaf422a40426ca9de0bba
Allester Corton <cortonallester@gmail.com>
2024-05-25 10:52:11 +0800

    added projects component

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -44,3 +44,46 @@ export const EXPERIENCES = [
     technologies: ['Java', 'Spring Boot', 'MySQL', 'JUnit'],
   },
 ]
+
+export const PROJECTS = [
+  {
+    title: 'E-commerce Website',
+    image:
+      'https://media.contra.com/image/upload/c_limit,fl_lossy,w_850/yzu0rxkq2wjdiux4og6v',
+    description:
+      'Built a fully functional e-commerce website with features such as product listing, shopping cart, and user authentication.',
+    technologies: ['React', 'Node.js', 'Express', 'MongoDB'],
+  },
+  {
+    title: 'Weather App',
+    image:
+      'https://uizard.io/static/89cb1d30cdee9c19b0ce72efe004ff49/a8e47/f5e31989f816c5b15c47cd54b0eb0b30b210b6f0-1440x835.png',
+    description:
+      'Developed a weather application that provides current weather conditions and forecasts for any location worldwide.',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'API'],
+  },
+  {
+    title: 'Task Management System',
+    image:
+      'https://cdn4.meistertask.com/assets/content/home/2021/ui-dashboard-6436b3719af91b5b986dcb405055f008fe8d61b09b336e7da3d59581e8eeb7fd.svg',
+    description:
+      'Created a task management system to organize and track tasks for individuals or teams, with features like task assignment and priority setting.',
+    technologies: ['Python', 'Django', 'SQLite'],
+  },
+  {
+    title: 'Social Media Platform',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQY6xIgTqoXD18PUEzaATjaIQPglBcEquGnFxTIK1LBog&s',
+    description:
+      'Designed and implemented a social media platform allowing users to connect, share content, and interact with each other.',
+    technologies: ['React', 'Node.js', 'MongoDB', 'WebSocket'],
+  },
+  {
+    title: 'Portfolio Website',
+    image:
+      'https://assets-global.website-files.com/589ed0903452732276fe0a7f/645cc6d101c00bc1f07e02a4_kyson-dana-portfolio-website.png',
+    description:
+      'Developed a personal portfolio website to showcase my projects, skills, and experience as a software engineer.',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'Bootstrap'],
+  },
+]
