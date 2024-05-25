4275583684bbc2748bdc0e9b38f80d7f069d643b
Allester Corton <cortonallester@gmail.com>
2024-05-25 10:06:07 +0800

    added experiences component

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,46 @@
-export const HERO_CONTENT = `Welcome to my digital playground! I'm a Full Stack Developer based in Ormoc City, Philippines, crafting dynamic web experiences that push the boundaries of what's possible. With PHP as my primary language and expertise spanning HTML, CSS, Tailwind, MERN stack, and Laravel, I thrive on turning ideas into interactive realities. Let's collaborate and bring your digital vision to life!`;
+export const HERO_CONTENT = `Welcome to my digital playground! I'm a Full Stack Developer based in Ormoc City, Philippines, crafting dynamic web experiences that push the boundaries of what's possible. With PHP as my primary language and expertise spanning HTML, CSS, Tailwind, MERN stack, and Laravel, I thrive on turning ideas into interactive realities. Let's collaborate and bring your digital vision to life!`
 
-export const ABOUT_CONTENT = `Hello! I'm Allester Corton, a 22 years old aspiring Software Engineer from Ormoc City, Philippines. Passionate about coding, I thrive on crafting elegant solutions to complex problems. Beyond the screen, you'll find me immersed in the exhilarating world of Formula 1, where my heart races alongside the Mercedes team. With a relentless drive for innovation and a penchant for precision, I'm committed to shaping the future through technology. As I navigate this journey, I live by the mantra: "Keep pushing to the absolute limit."`;
+export const ABOUT_CONTENT = `Hello! I'm Allester Corton, a 22 years old aspiring Software Engineer from Ormoc City, Philippines. Passionate about coding, I thrive on crafting elegant solutions to complex problems. Beyond the screen, you'll find me immersed in the exhilarating world of Formula 1, where my heart races alongside the Mercedes team. With a relentless drive for innovation and a penchant for precision, I'm committed to shaping the future through technology. As I navigate this journey, I live by the mantra: "Keep pushing to the absolute limit."`
+
+export const EXPERIENCES = [
+  {
+    year: '2018-2020',
+    role: 'Software Engineer Intern',
+    company: 'Tech Innovations Inc.',
+    description:
+      'Worked on various projects including developing new features, fixing bugs, and optimizing code.',
+    technologies: ['JavaScript', 'React', 'Node.js', 'MongoDB'],
+  },
+  {
+    year: '2019-2021',
+    role: 'Frontend Developer',
+    company: 'Web Solutions Co.',
+    description:
+      'Led frontend development for several client projects, ensuring high-quality user experiences.',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'Vue.js'],
+  },
+  {
+    year: '2020-2022',
+    role: 'Backend Developer',
+    company: 'Data Analytics Ltd.',
+    description:
+      'Designed and implemented scalable backend systems to support data processing pipelines.',
+    technologies: ['Python', 'Django', 'PostgreSQL', 'AWS'],
+  },
+  {
+    year: '2021-Present',
+    role: 'Full Stack Developer',
+    company: 'Innovative Startups LLC',
+    description:
+      'Contributing to the development of a cutting-edge web application from concept to deployment.',
+    technologies: ['JavaScript', 'React', 'Node.js', 'Express', 'MongoDB'],
+  },
+  {
+    year: '2017-2019',
+    role: 'Software Development Intern',
+    company: 'Tech Solutions Co.',
+    description:
+      'Assisted in the development and testing of software products, gaining hands-on experience in the software development lifecycle.',
+    technologies: ['Java', 'Spring Boot', 'MySQL', 'JUnit'],
+  },
+]
